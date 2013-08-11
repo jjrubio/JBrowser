bccd442981dc78fbe724da8b47997da4d09a42d1
Ramón Carrillo <racarrillo91@gmail.com>
2013-08-11 15:05:30 -0500

    Parse http responses

--- src/jbrowser/Http.java
+++ src/jbrowser/Http.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.LinkedHashMap;
 
 /**
  *
@@ -17,6 +18,11 @@ public class Http {
     private String uri;
     private String host;
     private int port;
+
+    // Http response
+    private int status;
+    private String content;
+    private LinkedHashMap<String, String> headers;
     
     // Socket connection
     private Socket socket;
@@ -30,7 +36,7 @@ public class Http {
         uri = url.split("://")[1].split("/", 2)[1];
    }
     
-    public String request() throws IOException {
+    public boolean request() throws IOException {
         
         try {
             socket = new Socket(host, port);
@@ -39,18 +45,50 @@ public class Http {
         } catch (Exception e) {
             System.out.println("ERROR: " + e.getMessage());
 
-            return "";
+            return false;
         }
 
         out.print(httpRequest());
         out.flush();
+        boolean ready = false;
 
-        String read, response = "";
-        while ( (read = in.readLine()) != null )
-            response += read + "\n";
+        // Look if there is response
+        // Try six times every 250 miliseconds
+        for( int tries = 1; tries <= 6; tries++) {
+            if (ready = in.ready())
+                break;
+            try {
+                Thread.sleep(250);
+            } catch (InterruptedException ex) {
+                return false;
+            }
+        }
 
-	    return response;
+        if (!ready) return false;
 
+        String[] header;
+
+        // Status
+        header = in.readLine().split(" ");
+        if (header.length != 3) return false;
+        status = Integer.parseInt(header[1]);
+
+        // Headers
+        String read;
+        headers = new LinkedHashMap<String, String>();
+        while ( (ready = in.ready()) && !(read = in.readLine()).equals("") ) {
+            if ( (header = read.split(": ", 2)).length != 2) return false;
+            headers.put(header[0], header[1]);
+        }
+
+        if (!ready) return false;
+
+        // Content
+        content = "";
+        while (in.ready())
+            content += in.readLine() + "\n";
+
+        return true;
     }
     
     private String httpRequest() {
@@ -59,5 +97,13 @@ public class Http {
                 "User-Agent: JBrower 0.0.1" + "\r\n" +
                 "Accept-Encoding: deflate" + "\r\n" +
                 "\r\n";
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public int getStatus() {
+        return status;
     }
 }
