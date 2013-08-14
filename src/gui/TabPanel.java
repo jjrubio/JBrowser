d823802964e8899f42afe6a63c5dbff4d89f6c43
Ramón Carrillo <racarrillo91@gmail.com>
2013-08-13 19:39:32 -0500

    Initial tab support

--- src/gui/TabPanel.java
+++ src/gui/TabPanel.java
@@ -1,40 +1,61 @@
-
 package gui;
 
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JEditorPane;
+import javax.swing.JLabel;
 import javax.swing.JTabbedPane;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 /**
  *
  * @author Jefferson Rubio <jefferson.jrubio@gmail.com>
  */
 public class TabPanel extends JTabbedPane{
+
+    JLabel newTab;
     
-    ButtonTabComponent btnTab;
-    
+    /**
+     * Add empty tab and "+" tab
+     *
+     * @author Jefferson Rubio <jefferson.jrubio@gmail.com>
+     * @author Ramón Carrillo <racarrillo91@gmail.com>
+     */
     public TabPanel(){
-        
+
         this.setUI(new PPTTabbedPaneUI());
         
-        this.addTab("New Tab", null);
-        
-        btnTab = new ButtonTabComponent("New Tab", this);
-        
-        this.setTabComponentAt(0,btnTab);
-        
-        this.addTab("+", null);
-        
-        this.addChangeListener(new ChangeListener() {
-            public void stateChanged(ChangeEvent e) {
-                if(getSelectedIndex() == getTabCount()-1){
-//                    btnTab = new ButtonTabComponent("New Tab", this);
-//                    setTabComponentAt(getSelectedIndex(),b);
-//                    setTitleAt(getSelectedIndex(), "New Tab");
-                    addTab("+", null);
-                }
-//              System.out.println("Tab=" + getSelectedIndex());
-//              System.out.println("Tabs=" + getTabCount());
+        newTab = new JLabel("+");
+        addTab("+", null);
+        setTabComponentAt(0, newTab);
+        getTabComponentAt(0).addMouseListener(new MouseAdapter() {
+
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                addNewTab();
             }
         });
+
+        addNewTab();
+    }
+
+    /**
+     * Add a new tab with an empty page
+     *
+     * @author Ramón Carrillo <racarrillo91@gmail.com>
+     */
+    private void addNewTab() {
+        // TODO create empty page
+        String html = "<html><body><p>it works!</p></body></html>";
+        JEditorPane page;
+        page = new JEditorPane();
+        page.setEditable(false);
+        page.setContentType("text/html");
+        page.setText(html);
+
+        int index = getTabCount() - 1;
+
+        insertTab("", null, page, "", index);
+
+        ButtonTabComponent btnTab = new ButtonTabComponent("New Tab", this);
+        setTabComponentAt(index, btnTab);
     }
 }
