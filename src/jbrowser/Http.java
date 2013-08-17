c8f27bb91c177affcaec02abe60fb5f543e60037
Ramón Carrillo <racarrillo91@gmail.com>
2013-08-16 19:10:00 -0500

    Cookies support

--- src/jbrowser/Http.java
+++ src/jbrowser/Http.java
@@ -1,6 +1,11 @@
 package jbrowser;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.File;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
@@ -29,6 +34,9 @@ public class Http {
     private BufferedReader in = null;
 
     private String errorMsg;
+
+    // Cookies
+    private LinkedHashMap<String, String> cookies;
     
     Http(String url) {
         host = url.split("://")[1].split("/")[0];
@@ -44,6 +52,8 @@ public class Http {
             socket = new Socket(host, port);
             out = new PrintWriter(socket.getOutputStream(), true);
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            loadCookies();
 
             out.print(httpRequest());
             out.flush();
@@ -85,6 +95,7 @@ public class Http {
                     errorMsg = "Bad-formed HTTP response: Header is wrong.";
                     return false;
                 }
+                processHeader(header[0], header[1]);
                 headers.put(header[0], header[1]);
             }
 
@@ -92,6 +103,8 @@ public class Http {
                 errorMsg = "Bad-formed HTTP response: Headers are incomplete. ";
                 return false;
             }
+
+            saveCookies();
 
             // Content
             content = "";
@@ -116,6 +129,7 @@ public class Http {
                 "Host: " + host + "\r\n" +
                 "User-Agent: JBrower 0.0.1" + "\r\n" +
                 "Accept-Encoding: deflate" + "\r\n" +
+                cookies() +
                 "\r\n";
     }
 
@@ -125,5 +139,89 @@ public class Http {
 
     public int getStatus() {
         return status;
+    }
+
+    public String getHeader(String header) {
+	return headers.get(header);
+    }
+
+    private void processHeader(String header, String value) {
+
+        if (header.equals("Set-Cookie")) {
+            String[] cookie = value.split(";")[0].split("=");
+
+            // Consider empty cookies
+            if (cookie.length == 1)
+                cookies.put(cookie[0], "");
+            else if (cookie.length == 2)
+                cookies.put(cookie[0], cookie[1]);
+
+        }
+    }
+
+    private String cookies() {
+        String cookiesToSend = "";
+
+        if ( cookies.size() == 0 )
+	    return "";
+
+        cookiesToSend = "Cookie: ";
+
+        int i = 0;
+        for(String cookie : cookies.keySet()) {
+
+            if (i != 0)
+                cookiesToSend += "; ";
+
+            cookiesToSend += cookie + "=" + cookies.get(cookie);
+
+            i++;
+        }
+
+        cookiesToSend += "\r\n";
+
+        return cookiesToSend;
+    }
+
+    private void saveCookies() {
+        File cookiesDir = new File("cookies");
+
+        if ( !cookiesDir.isDirectory() )
+            cookiesDir.mkdir();
+
+        BufferedWriter out = null;
+        try {
+            out = new BufferedWriter( new FileWriter("cookies/" + host, false) );
+
+            for(String cookie : cookies.keySet()) {
+                out.write(cookie + "=" + cookies.get(cookie) + " \n");
+            }
+
+            out.close();
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    private void loadCookies() {
+	cookies = new LinkedHashMap<String, String>();
+        FileReader f = null;
+
+	if ( !( new File("cookies/" + host) ).isFile() )
+	    return;
+
+        try {
+            f = new FileReader("cookies/" + host);
+
+            BufferedReader in = new BufferedReader(f);
+            String line;
+            while ( ( line = in.readLine() ) != null ) {
+                String[] cookie = line.split("=", 2);
+                if (cookie.length == 2)
+                    cookies.put(cookie[0], cookie[1]);
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 }
